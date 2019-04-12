a80056f7933d199bbf5645022850fd25a4fe29da
Ashley Brandon <ashley.brandon@ad.sequelgroup.co.uk>
2019-04-12 17:21:56 +0100

    notes n regex |

--- app.js
+++ app.js
@@ -25,29 +25,57 @@ const coinsDash = document.querySelector('.coins-dash');
 document.querySelector('.input-form').addEventListener('submit', function(e) {    
 
 const newInput = inputField.value.replace(/p/ , '');
-const poundsTot = /£/;
+const poundsTot = /£|\d\.\d\d/;
+const zeroPointPence = /(0)\.*/;
 const decP = /\./;
 const rawNumber = parseInt(newInput.match(/\d+/),10);
-const poundsInPence = rawNumber * 100; 
+const poundsInPence = rawNumber * 100;
+const downToPence = rawNumber / 100;
+//one that divides by 100 if pence
+
+
+
+zeroPointPence.test(newInput) === true ? 
+coinsDash.innerHTML = `<h1>${getCoins(downToPence)}</h1>` : 
+coinsDash.innerHTML = `<h1>Enter amount in Sterling</h1>`;
 
 //checks if input contains decimal point
-decP.test(newInput) === true ? console.log('yes') : console.log('no');
-
 if(poundsTot.test(newInput) === true) {
 
+
+//interprets input intention as £
     coinsDash.innerHTML = `<h1>${getCoins(poundsInPence)}</h1>`;
 
 } else if(isNaN(newInput)) {
 
+    //handles NaN errors
     coinsDash.innerHTML = `<h1>Enter amount in Sterling</h1>`;
 
 } else {
+
+    //interprets input intention as pence
     coinsDash.innerHTML = `<h1>${getCoins(newInput)}</h1>`;
 }
 
+
+
+
+//decP.test(newInput) === true ? console.log('yes') : console.log('no');
 /*
+
+now that we can add or/ || in regex - we could probs define pounds or pence in that
+
+
 - We possibly need a switch statement to handle all req scenarios
 - otherwise we could use the ? : to check for decimals, which I can't get to work in current structure.
+
+alternatively you could make an array of regex objects, and a loop that says if there's a match, output that one
+
+
+---12/04/2019
+- you can't have conditional cases in switch statements. switches only test strings, numbers, specific values etc.
+- At this point it seems like you need to define more scenarios as variables and say || or && in the if/else
+
 */
 
 e.preventDefault();
